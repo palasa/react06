969b833e9b176be0709ef26f8844f79d065a616c
palasa <170394205@qq.com>
2022-05-04 18:20:35 +0800

    router

--- src/views/Article/ArticleDetail.js
+++ src/views/Article/ArticleDetail.js
@@ -1,10 +1,21 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
+import { BackButton } from '../../components'
 
 
 export default function ArticleDetail() {
+
   const params = useParams()
+  const navigate = useNavigate()
+
   // console.log("articleDetail: ", +params.id)
   return (
-    <div>文章详情 {params.id}</div>
+    <>
+      <div>文章详情 {params.id}</div>
+      <button onClick={() => navigate(-1)}>go back</button>
+      <button onClick={() => navigate(1)}>go forward</button>
+      <button onClick={() => navigate('/')}>返回首页</button>
+      <button onClick={() => navigate('/users')}>跳转到users</button>
+      <BackButton />
+    </>
   )
 }
